a24a1e115f5f7f2457ab2c5a312b300fe63f3bea
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-07 14:35:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
 var api_key = "5t7DjXqJjYUeYWgsjvFr4vaw8xjRZYvCZtXTNyll";
 var date_req = "&date=";
-var date = "2022-01-08";
+var date = 2022-01-08;
 req.open("GET", url + api_key + date_req + date);
 req.send();
 
