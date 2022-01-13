f073245ab04c3f4d59be8bdd88e553fa21a34019
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-12 18:33:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ var date = "2022-01-06";
 req.open("GET", url + api_key + date_req + date);
 req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-    document.getElementById("title").textContent = response.title;
-    document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
-    document.getElementById("explanation").textContent = response.explanation;
-  }
-})
+// req.addEventListener("load", function(){
+// 	if(req.status == 200 && req.readyState == 4){
+//   	var response = JSON.parse(req.responseText);
+//     document.getElementById("title").textContent = response.title;
+//     document.getElementById("date").textContent = response.date;
+//     document.getElementById("pic").src = response.hdurl;
+//     document.getElementById("explanation").textContent = response.explanation;
+//   }
+// })
 
 
 function generate(){
