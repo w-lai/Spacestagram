8099e5d2de2471e9fff3fcdc8c371a1437f58b66
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-13 18:10:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,20 @@ function generate(){
 	var api_key = "5t7DjXqJjYUeYWgsjvFr4vaw8xjRZYvCZtXTNyll";
 	var date_req = "&date=";
 	var date = "2022-01-06";
-	fetch(url + api_key + date_req + date).then(response => {
+	fetch(url + api_key + "&count=5").then(response => {
 		if (!response.ok) {
 			throw Error("error");
 		}
 		return response.json();
 	}).then(data => {
-		const stuff = data.data;
+		const stuff = data.data.map(user=> {
+			return `<div class="post">
+			<p>$[user.title]</p>
+			<p>$[user.date]</p>
+			<p><img src="$[user.hdurl]"/></p>
+			<p>$[user.explanation]</p>
+			</div>`;
+		}).join("");
 	document.querySelector("#posts").insertAdjacentHTML("afterbegin", stuff);
 	})
 }
