3ff7aa31994db03fac21468cf85c481e206fbf07
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-13 18:15:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ function generate(){
 	}).then(data => {
 		const stuff = data.map(user=> {
 			return `<div class="post">
-			<p>$[user.title]</p>
-			<p>$[user.date]</p>
-			<p><img src="$[user.hdurl]"/></p>
-			<p>$[user.explanation]</p>
+			<p>$[stuff.title]</p>
+			<p>$[stuff.date]</p>
+			<p><img src="$[stuff.hdurl]"/></p>
+			<p>$[stuff.explanation]</p>
 			</div>`;
 		}).join("");
 	document.querySelector("#posts").insertAdjacentHTML("afterbegin", stuff);
