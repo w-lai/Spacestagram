ea69ea78c0a48d05bfa12fe4c36853b880a924ea
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-13 17:54:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,26 +14,26 @@
 
 
 function generate(){
-	
-	var req = new XMLHttpRequest();
 	var url = "https://api.nasa.gov/planetary/apod?api_key=";
 	var api_key = "5t7DjXqJjYUeYWgsjvFr4vaw8xjRZYvCZtXTNyll";
 	var date_req = "&date=";
 	var date = "2022-01-06";
-	req.open("GET", url + api_key + date_req + date);
-	req.send();
-
-  	var response = JSON.parse(req.responseText);
-	const para = document.createElement("a");
-	const node = document.createTextNode("hello");
-	const node1 = document.createTextNode(response.date);
-	const node3 = document.createTextNode(response.explanation);
-	var node2 = document.createElement("img");
-	node2.src = response.hdurl;
-
-	para.appendChild(node);
-	para.appendChild(node1);
-	para.appendChild(node2);
-	para.appendChild(node3);
-	
+	fetch(url + api_key + date_req + date).then(response => {
+		if (!response.ok) {
+			throw Error("error");
+		}
+		return response.json();
+	}).then(data => {
+		const stuff = data.data.map(user => {
+			return `<div class="post">
+			<p>$[user.title]</p>
+			<p>$[user.date]</p>
+			<p><img src="$[user.hdurl]"/></p>
+			<p>$[user.explanation]</p>
+			</div>`;
+		}).join("");
+	document.querySelector("#posts").insertAdjacentHTML("afterbegin", stuff);
+	})
 }
+	
+
