5febbeac278611e668f3fcd165662635358fe5bb
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-13 23:56:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function generate(){
 	var api_key = "5t7DjXqJjYUeYWgsjvFr4vaw8xjRZYvCZtXTNyll";
 	var date_req = "&date=";
 	var date = "2022-01-06";
+	var idh = "heart";
 	fetch(url + api_key + "&count=5").then(response => {
 		if (!response.ok) {
 			throw Error("error");
@@ -31,9 +32,9 @@ function generate(){
 			<p id="title">${user.title}</p>
 			<p>${user.date}</p>
 			<p><img src="${user.hdurl}" width="350" height= "250" /></p>
-			<p hidden id="heart"><img src="Heart.png" width="350" height= "250" /></p>
+			<p hidden id=idh><img src="Heart.png" width="350" height= "250" /></p>
 			<p>${user.explanation}</p>
-			<button onclick="like(this, "heart")">Like</button>
+			<button onclick="like(this, idh)">Like</button>
 			</div>`;
 		}).join("");
 	document.querySelector("#posts").insertAdjacentHTML("afterbegin", stuff);
