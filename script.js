5a27e530d76c47bbf3d057672814b2e9259308d7
w-lai <90171336+w-lai@users.noreply.github.com>
2022-01-13 21:33:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function generate(){
 			return `<div class="post">
 			<p id="title">${user.title}</p>
 			<p>${user.date}</p>
-			<p><img src="${user.hdurl}" width="150" height= "150" /></p>
+			<p><img src="${user.hdurl}" width="300" height= "250" /></p>
 			<p>${user.explanation}</p>
 			<button onclick="like(this)">Like</button>
 			</div>`;
